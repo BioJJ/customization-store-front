b1c659279d49123258faa5c4f091bcc14e9a3bf5
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-10-02 17:38:28 -0400

    feat: ✨ add router

---
+++ src/routes/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { RootState } from '../store'
+
+import App from './app.routes'
+import Auth from './auth.routes'
+
+const Routes: React.FC = () => {
+	const isLogged = useSelector(
+		(rootState: RootState) => !!rootState.globalReducer.user?.id
+	)
+
+	console.log(isLogged)
+	return <BrowserRouter>{isLogged ? <App /> : <Auth />}</BrowserRouter>
+}
+
+export default Routes
